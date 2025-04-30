25b8d0ccaa9d4f197278b95374113f020bff69af
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-04-30 11:24:35 +0200

    add: snack 1.1 funzione dichiarativa

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 // start code
+
+// SNACK 1 ----------------------------
+
+// 1. funzione dichiarativa somma
+function sommaDichiarativa(num1, num2) {
+  console.log(`${num1} + ${num2} = `, num1 + num2);
+}
+
+sommaDichiarativa(2, 4);
