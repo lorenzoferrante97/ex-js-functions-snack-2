0a0826f9b79044de99eb32c5153dba0b0102b12d
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-04-30 11:29:52 +0200

    add: snack 1.3 arrow function

--- script.js
+++ script.js
@@ -12,5 +12,11 @@ const sommaAnonima = function (num1, num2) {
   console.log(`${num1} + ${num2} = `, num1 + num2);
 };
 
+// 3. arrow function
+const sommaArrow = (num1, num2) => {
+  console.log(`${num1} + ${num2} = `, num1 + num2);
+};
+
 sommaDichiarativa(2, 4);
 sommaAnonima(3, 3);
+sommaArrow(1, 5);
