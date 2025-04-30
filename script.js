bdb2b43463bf5327726e8e14a40bb743873c8049
lorenzoferrante97 <lorenzoferrante.design@gmail.com>
2025-04-30 11:34:15 +0200

    add: snack 2 arrow function quadrato

--- script.js
+++ script.js
@@ -20,3 +20,10 @@ const sommaArrow = (num1, num2) => {
 sommaDichiarativa(2, 4);
 sommaAnonima(3, 3);
 sommaArrow(1, 5);
+
+// SNACK 2 ----------------------------
+
+// quadrato di un num su una riga
+const quadrato = (num) => console.log(`quadrato di ${num}: `, num * num);
+
+quadrato(3);
